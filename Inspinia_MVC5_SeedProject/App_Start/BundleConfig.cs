932249ab1459e893856baaa2282549dfc3af7dc8
Cristian Sanchez <alexandersmz92@gmail.com>
2023-11-14 13:26:45 -0600

    Datatable updated

--- Inspinia_MVC5_SeedProject/App_Start/BundleConfig.cs
+++ Inspinia_MVC5_SeedProject/App_Start/BundleConfig.cs
@@ -10,60 +10,56 @@ namespace FARSIMAN
         {
             // CSS style (bootstrap/inspinia)
             bundles.Add(new StyleBundle("~/Content/Style").Include(
-                      "~/Content/bootstrap.old.min.css",
-                      "~/Content/animate.css",
-                      "~/Content/style.css"));
+                            "~/Content/bootstrap.min.css",
+                            "~/Content/animate.css",
+                            "~/Content/style.css"));
 
-            bundles.Add(new StyleBundle("~/Content/DataTabla")
-                .Include("~/Content/DataTables/datatables.min.css"));
+            
             // Font Awesome icons
             bundles.Add(new StyleBundle("~/Content/font-Style").Include(
-                      "~/fonts/font-awesome/css/all.min.css", new CssRewriteUrlTransform()));
+                            "~/fonts/font-awesome/css/all.min.css", new CssRewriteUrlTransform()));
 
             //Date picker
             bundles.Add(new StyleBundle("~/Content/picker").Include(
-                        "~/Content/themes/base/jquery-ui.min.css"
-            ));
+                            "~/Content/themes/base/jquery-ui.min.css"));
 
             // Vendor scripts
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-3.7.1.min.js"));
+                            "~/Scripts/jquery-3.7.1.min.js"));
 
             // jQuery Validation
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            "~/Scripts/jquery.validate.min.js"));
+                            "~/Scripts/jquery.validate.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.old.min.js",
-                      "~/Scripts/datatables.min.js"));
+                            "~/Scripts/bootstrap.old.min.js"));
 
             // Inspinia script
             bundles.Add(new ScriptBundle("~/bundles/inspinia").Include(
-                      "~/Scripts/app/inspinia.js"));
+                            "~/Scripts/app/inspinia.js"));
 
             // SlimScroll
             bundles.Add(new ScriptBundle("~/plugins/slimScroll").Include(
-                      "~/Scripts/plugins/slimscroll/jquery.slimscroll.min.js"));
+                            "~/Scripts/plugins/slimscroll/jquery.slimscroll.min.js"));
 
             // jQuery plugins
             bundles.Add(new ScriptBundle("~/plugins/Menu").Include(
-                      "~/Scripts/plugins/metisMenu/metisMenu.min.js"));
+                            "~/Scripts/plugins/metisMenu/metisMenu.min.js"));
 
             bundles.Add(new ScriptBundle("~/plugins/pace").Include(
-                      "~/Scripts/plugins/pace/pace.min.js"));
+                            "~/Scripts/plugins/pace/pace.min.js"));
 
-           
+            // Datatable
+            bundles.Add(new StyleBundle("~/Content/DataTabla").Include(
+                            "~/Content/DataTables/datatables.min.css"));
 
-            bundles.Add(new ScriptBundle("~/Scripts/DataTabla")
-                      .Include(
-                            "~/Scripts/DataTables/jquery.dataTables.min.js",
+            bundles.Add(new ScriptBundle("~/Scripts/DataTabla").Include(
+                            "~/Scripts/DataTables/datatables.min.js",
                             "~/Scripts/DataTables/dataTables.responsive.min.js",
-                            "~/Scripts/DataTables/dataTables.bootstrap.min.js"
-                ));
-            
+                            "~/Scripts/DataTables/dataTables.bootstrap5.min.js"));
+
             bundles.Add(new ScriptBundle("~/Scripts/picker").Include(
-                "~/Scripts/jquery-ui-1.13.2.min.js"
-                ));
+                            "~/Scripts/jquery-ui-1.13.2.min.js"));
 
         }
     }
--- Inspinia_MVC5_SeedProject/Scripts/Datatables/dataTables.bootstrap5.js
+++ Inspinia_MVC5_SeedProject/Scripts/Datatables/dataTables.bootstrap5.js
@@ -1,5 +1,5 @@
-/*! DataTables Bootstrap 3 integration
- * ©2011-2015 SpryMedia Ltd - datatables.net/license
+/*! DataTables Bootstrap 5 integration
+ * 2020 SpryMedia Ltd - datatables.net/license
  */
 
 (function( factory ){
@@ -50,7 +50,8 @@ var DataTable = $.fn.dataTable;
 
 
 /**
- * DataTables integration for FomanticUI (formally SemanticUI)
+ * DataTables integration for Bootstrap 5. This requires Bootstrap 5 and
+ * DataTables 1.10 or newer.
  *
  * This file sets the defaults and adds options to DataTables to style its
  * controls using Bootstrap. See https://datatables.net/manual/styling/bootstrap
@@ -60,34 +61,25 @@ var DataTable = $.fn.dataTable;
 /* Set the defaults for DataTables initialisation */
 $.extend( true, DataTable.defaults, {
 	dom:
-		"<'ui stackable grid'"+
-			"<'row'"+
-				"<'eight wide column'l>"+
-				"<'right aligned eight wide column'f>"+
-			">"+
-			"<'row dt-table'"+
-				"<'sixteen wide column'tr>"+
-			">"+
-			"<'row'"+
-				"<'seven wide column'i>"+
-				"<'right aligned nine wide column'p>"+
-			">"+
-		">",
-	renderer: 'semanticUI'
+		"<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+		"<'row dt-row'<'col-sm-12'tr>>" +
+		"<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+	renderer: 'bootstrap'
 } );
 
 
 /* Default class modification */
 $.extend( DataTable.ext.classes, {
-	sWrapper:      "dataTables_wrapper dt-semanticUI",
-	sFilter:       "dataTables_filter ui input",
-	sProcessing:   "dataTables_processing ui segment",
-	sPageButton:   "paginate_button item"
+	sWrapper:      "dataTables_wrapper dt-bootstrap5",
+	sFilterInput:  "form-control form-control-sm",
+	sLengthSelect: "form-select form-select-sm",
+	sProcessing:   "dataTables_processing card",
+	sPageButton:   "paginate_button page-item"
 } );
 
 
 /* Bootstrap paging button renderer */
-DataTable.ext.renderer.pageButton.semanticUI = function ( settings, host, idx, buttons, page, pages ) {
+DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, buttons, page, pages ) {
 	var api     = new DataTable.Api( settings );
 	var classes = settings.oClasses;
 	var lang    = settings.oLanguage.oPaginate;
@@ -150,27 +142,28 @@ DataTable.ext.renderer.pageButton.semanticUI = function ( settings, host, idx, b
 						break;
 				}
 
-				var disabled = btnClass.indexOf('disabled') !== -1;
-				var tag = disabled ?
-					'div' :
-					'a';
-
 				if ( btnDisplay ) {
-					node = $('<'+tag+'>', {
+					var disabled = btnClass.indexOf('disabled') !== -1;
+
+					node = $('<li>', {
 							'class': classes.sPageButton+' '+btnClass,
 							'id': idx === 0 && typeof button === 'string' ?
 								settings.sTableId +'_'+ button :
-								null,
-							'href': disabled ? null : '#',
-							'aria-controls': settings.sTableId,
-							'aria-disabled': disabled ? 'true' : null,
-							'aria-label': aria[ button ],
-							'role': 'link',
-							'aria-current': btnClass === 'active' ? 'page' : null,
-							'data-dt-idx': button,
-							'tabindex': disabled ? -1 : settings.iTabIndex
+								null
 						} )
-						.html( btnDisplay )
+						.append( $('<a>', {
+								'href': disabled ? null : '#',
+								'aria-controls': settings.sTableId,
+								'aria-disabled': disabled ? 'true' : null,
+								'aria-label': aria[ button ],
+								'role': 'link',
+								'aria-current': btnClass === 'active' ? 'page' : null,
+								'data-dt-idx': button,
+								'tabindex': disabled ? -1 : settings.iTabIndex,
+								'class': 'page-link'
+							} )
+							.html( btnDisplay )
+						)
 						.appendTo( container );
 
 					settings.oApi._fnBindAction(
@@ -181,6 +174,7 @@ DataTable.ext.renderer.pageButton.semanticUI = function ( settings, host, idx, b
 		}
 	};
 
+	var hostEl = $(host);
 	// IE9 throws an 'unknown error' if document.activeElement is used
 	// inside an iframe or frame. 
 	var activeEl;
@@ -190,38 +184,28 @@ DataTable.ext.renderer.pageButton.semanticUI = function ( settings, host, idx, b
 		// elements, focus is lost on the select button which is bad for
 		// accessibility. So we want to restore focus once the draw has
 		// completed
-		activeEl = $(host).find(document.activeElement).data('dt-idx');
+		activeEl = hostEl.find(document.activeElement).data('dt-idx');
 	}
 	catch (e) {}
 
+	var paginationEl = hostEl.children('ul.pagination');
+
+	if (paginationEl.length) {
+		paginationEl.empty();
+	}
+	else {
+		paginationEl = hostEl.html('<ul/>').children('ul').addClass('pagination');
+	}
+
 	attach(
-		$(host).empty().html('<div class="ui stackable pagination menu"/>').children(),
+		paginationEl,
 		buttons
 	);
 
 	if ( activeEl !== undefined ) {
-		$(host).find( '[data-dt-idx='+activeEl+']' ).trigger('focus');
+		hostEl.find('[data-dt-idx='+activeEl+']').trigger('focus');
 	}
 };
-
-
-// Javascript enhancements on table initialisation
-$(document).on( 'init.dt', function (e, ctx) {
-	if ( e.namespace !== 'dt' ) {
-		return;
-	}
-
-	var api = new $.fn.dataTable.Api( ctx );
-
-	// Length menu drop down
-	if ( $.fn.dropdown ) {
-		$( 'div.dataTables_length select', api.table().container() ).dropdown();
-	}
-
-	// Filtering input
-	$( 'div.dataTables_filter.ui.input', api.table().container() ).removeClass('input').addClass('form');
-	$( 'div.dataTables_filter input', api.table().container() ).wrap( '<span class="ui input" />' );
-} );
 
 
 return DataTable;
